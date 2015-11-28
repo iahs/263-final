5b75541ff34bb85ef6f4609878d6d469c77e7780
Peter Kraft <pkraft@college.harvard.edu>
2015-11-28 14:50:16 -0500

    Added vulnerability location detection

--- hackmelists/lists/tests.py
+++ hackmelists/lists/tests.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import time
 
-XSS_STRINGS = ['<image src="doesnotexist" onerror=alert("bob")></img>']
+XSS_STRINGS = ['<image src="doesnotexist" onerror=alert("HACKED12345")></img>']
 
 class MySeleniumTests(StaticLiveServerTestCase):
 
@@ -35,6 +35,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
                 buttons = self.selenium.find_elements(By.XPATH, "//*[@onclick]")
                 txtfields = self.selenium.find_elements(By.XPATH, "//input[@type='text']") + \
                     self.selenium.find_elements(By.XPATH, "//textarea")
+                numfields = len(txtfields)
                 for field in txtfields:
                     field.send_keys(attackstring)
                 buttons[i].click()
@@ -45,7 +46,37 @@ class MySeleniumTests(StaticLiveServerTestCase):
 
                     alert = self.selenium.switch_to_alert()
                     alert.accept()
-                    print "VULNERABILITY FOUND:", attackstring
+                    for j in range(numfields):
+                        try:
+                            alert = self.selenium.switch_to_alert()
+                            alert.accept()
+                        except:
+                            pass
+                        self.selenium.refresh()
+                        self.selenium.get('%s%s' % (self.live_server_url, '/list/'))
+                        buttons = self.selenium.find_elements(By.XPATH, "//*[@onclick]")
+                        txtfields = self.selenium.find_elements(By.XPATH, "//input[@type='text']") + \
+                            self.selenium.find_elements(By.XPATH, "//textarea")
+                        txtfields[j].send_keys(attackstring)
+                        buttons[i].click()
+                        try:
+                            WebDriverWait(self.selenium, 1).until(EC.alert_is_present(),
+                                                           'Timed out waiting for PA creation ' +
+                                                           'confirmation popup to appear.')
+
+                            alert = self.selenium.switch_to_alert()
+                            if alert.text == "HACKED12345":
+                                alert.accept()
+                                print "VULNERABILITY FOUND"
+                                print "To attack string:", attackstring
+                                print "On button of type:", buttons[i].tag_name, "  At point:", buttons[i].location, "  Showing text (if any):", buttons[i].text
+                                print "On textfield of type:", txtfields[j].tag_name, "  At point:", txtfields[j].location, "  Showing text (if any):", txtfields[j].text
+                                print
+                            else:
+                                alert.accept()
+                        except TimeoutException:
+                            pass
                 except TimeoutException:
                     print "no alert"
+                    print
                 self.selenium.refresh()
